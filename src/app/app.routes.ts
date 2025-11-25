f0a2b75f816bbd0a28f2484d0fca2ab2a8f60202
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-11-25 11:24:52 +0100

    added files and images

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Hero } from './hero/hero';
+import { Order } from './order/order';
+import { OurMenu } from './our-menu/our-menu';
+import { FindUs } from './find-us/find-us';
+import { Footer } from './shared/footer/footer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'hero', pathMatch: 'full' },
+  { path: 'hero', component: Hero },
+  { path: 'order', component: Order },
+  { path: 'our-menu', component: OurMenu },
+  { path: 'find-us', component: FindUs },
+  { path: 'footer', component: Footer }
+];
+
---
+++ src/app/find-us/find-us.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-us',
+  imports: [],
+  templateUrl: './find-us.html',
+  styleUrl: './find-us.sass',
+})
+export class FindUs {
+
+}
