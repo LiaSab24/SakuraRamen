632413adb558617c1623924602472afc3c5594cc
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-12-02 10:20:47 +0100

    changed code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { Hero } from './hero/hero';
+import { Order } from './order/order';
+import { OurMenu } from './our-menu/our-menu';
+import { FindUs } from './find-us/find-us';
+import { Footer } from './shared/footer/footer';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, Hero, Order, OurMenu, FindUs, Footer],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
