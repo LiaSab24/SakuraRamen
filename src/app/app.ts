b4fdd455b363eeeb785332b6deb56d87bd5ebd8c
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-12-17 10:27:25 +0100

    clearing app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
-//import { MainContent } from './mainContent/mainContent';
-//import { Imprint } from './imprint/imprint';
 import { Footer } from './shared/footer/footer';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule,  Footer], //MainContent, Imprint,
+  imports: [RouterOutlet, RouterModule,  Footer],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
